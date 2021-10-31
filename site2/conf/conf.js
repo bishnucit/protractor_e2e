3fcbb4afd44b66229bb421b4a8b65cd8a2d71b3c
Bishnu Prasad Chowdhury <bishnucit@gmail.com>
2021-10-31 09:36:28 +0530

    Update conf.js

--- site2/conf/conf.js
+++ site2/conf/conf.js
@@ -1,13 +1,52 @@
 exports.config = {
+
+    onPrepare: function(){
+        var jasmineReporters = require('jasmine-reporters');
+        jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter({
+            consolidateAll: true,
+            savePath: './',
+            filePrefix: 'xmlresults'
+        }));
+    },
+
+    onComplete: function() {
+        var browserName, browserVersion;
+        var capsPromise = browser.getCapabilities();
+        capsPromise.then(function (caps) {
+           browserName = caps.get('browserName');
+           browserVersion = caps.get('version');
+           platform = caps.get('platform');
+            var HTMLReport = require('protractor-html-reporter-2');
+            testConfig = {
+                reportTitle: 'Protractor Test Execution Report',
+                outputPath: './',
+                outputFilename: 'ProtractorTestReport',
+                screenshotPath: './screenshots',
+                testBrowser: browserName,
+                browserVersion: browserVersion,
+                modifiedSuiteName: false,
+                screenshotsOnlyOnFailure: true,
+                testPlatform: platform
+            };
+            new HTMLReport().from('xmlresults.xml', testConfig);
+       });
+    },
+
     directConnect: true,
 
-    capabilities: {
+    multiCapabilities: [
+        {
         'browserName': 'chrome'
-    },
+        },
+        {
+         'browserName': 'firefox'
+        },
+
+    ], maxSessions: -1,
 
     framework: 'jasmine',
 
-    specs: ['..//testcases//verify_all_elements.js'],
+    specs: ['..//testcases//verify_all_elements.js','..//testcases//registration.js'],
 
     jasmineNodeOpts: {
         defaultTimeoutInterval: 30000
