829de1025d6e38048011b0d816a88379cc42fe5f
Bishnu Prasad Chowdhury <bishnucit@gmail.com>
2021-11-03 14:34:13 +0530

    Update login.js

--- site2/testcases/login.js
+++ site2/testcases/login.js
@@ -12,19 +12,12 @@ describe("logins the registered user", function(){
         .then(function() {
             var loggedin_message = element(by.xpath('/html/body/app/div[1]/div/div/div/ng-component/p'))
             expect(loggedin_message.isDisplayed());
-        })
-        .catch(function(err){
-            console.error("Register button element is not found! ", err);
         });
-
         element(by.xpath('/html/body/app/nav/div/a[2]'))
         .click()
         .then(function() {
             var login_page = element(by.buttonText('Login'))
             expect(login_page.isDisplayed());
-        })
-        .catch(function(err){
-            console.error("Logout element is not found! ", err);
         });
     });
 
@@ -40,19 +33,12 @@ describe("logins the registered user", function(){
         .then(function() {
             var loggedin_message = element(by.xpath('/html/body/app/div[1]/div/div/div/ng-component/p'))
             expect(loggedin_message.isDisplayed());
-        })
-        .catch(function(err){
-            console.error("Register button element is not found! ", err);
         });
-
         element(by.xpath('/html/body/app/nav/div/a[2]'))
         .click()
         .then(function() {
             var login_page = element(by.buttonText('Login'))
             expect(login_page.isDisplayed());
-        })
-        .catch(function(err){
-            console.error("Logout element is not found! ", err);
         });
         browser.sleep(2000);
         element(by.xpath('/html/body/app/div[1]/div/div/div/ng-component/form/div[1]/input')).clear().sendKeys('tester1');
@@ -63,10 +49,6 @@ describe("logins the registered user", function(){
         .then(function() {
             var loggedin_message = element(by.xpath('/html/body/app/div[1]/div/div/div/ng-component/p'))
             expect(loggedin_message.isDisplayed());
-        })
-        .catch(function(err){
-            console.error("Register button element is not found! ", err);
         });
-
     });
 });
