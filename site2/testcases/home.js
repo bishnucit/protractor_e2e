791eebf5add917208242f77ab8178135b70971ed
Bishnu Prasad Chowdhury <bishnucit@gmail.com>
2021-11-04 13:29:05 +0530

    Create home.js

---
+++ site2/testcases/home.js
@@ -0,0 +1,61 @@
+describe("logins the registered user", function(){
+
+    it("TC001 - Login with the registered user", function(){
+        browser.get('http://localhost:8080/login');
+        browser.manage().window().maximize();
+        browser.sleep(2000);
+        element(by.xpath('/html/body/app/div[1]/div/div/div/ng-component/form/div[1]/input')).clear().sendKeys('tester1');
+        element(by.xpath('/html/body/app/div[1]/div/div/div/ng-component/form/div[2]/input')).clear().sendKeys('P@ssW0rD123');
+        browser.sleep(2000);
+        element(by.buttonText('Login'))
+        .click()
+        .then(function() {
+            var loggedin_message = element(by.xpath('/html/body/app/div[1]/div/div/div/ng-component/p'))
+            expect(loggedin_message.isDisplayed());
+        });
+        element(by.xpath('/html/body/app/nav/div/a[2]'))
+        .click()
+        .then(function() {
+            var login_page = element(by.buttonText('Login'))
+            expect(login_page.isDisplayed());
+        });
+    });
+
+    it("TC002 - Logout the user and relogin the same user", function(){
+        browser.get('http://localhost:8080/login');
+        browser.manage().window().maximize();
+        browser.sleep(2000);
+        element(by.xpath('/html/body/app/div[1]/div/div/div/ng-component/form/div[1]/input')).clear().sendKeys('tester1');
+        element(by.xpath('/html/body/app/div[1]/div/div/div/ng-component/form/div[2]/input')).clear().sendKeys('P@ssW0rD123');
+        browser.sleep(2000);
+        element(by.buttonText('Login'))
+        .click()
+        .then(function() {
+            var loggedin_message = element(by.xpath('/html/body/app/div[1]/div/div/div/ng-component/p'))
+            expect(loggedin_message.isDisplayed());
+        });
+        element(by.xpath('/html/body/app/nav/div/a[2]'))
+        .click()
+        .then(function() {
+            var login_page = element(by.buttonText('Login'))
+            expect(login_page.isDisplayed());
+        });
+        browser.sleep(2000);
+        element(by.xpath('/html/body/app/div[1]/div/div/div/ng-component/form/div[1]/input')).clear().sendKeys('tester1');
+        element(by.xpath('/html/body/app/div[1]/div/div/div/ng-component/form/div[2]/input')).clear().sendKeys('P@ssW0rD123');
+        browser.sleep(2000);
+        element(by.buttonText('Login'))
+        .click()
+        .then(function() {
+            var loggedin_message = element(by.xpath('/html/body/app/div[1]/div/div/div/ng-component/p'))
+            expect(loggedin_message.isDisplayed());
+        });
+        browser.sleep(1000);
+        element(by.xpath('/html/body/app/nav/div/a[2]'))
+        .click()
+        .then(function() {
+            var login_page = element(by.buttonText('Login'))
+            expect(login_page.isDisplayed());
+        });
+    });
+});
