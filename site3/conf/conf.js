400d6ed0d3d64eb1e7766beb1cabc6bd2b4ab9c7
Bishnu Prasad Chowdhury <bishnucit@gmail.com>
2021-11-08 16:16:10 +0530

    Create conf.js

---
+++ site3/conf/conf.js
@@ -0,0 +1,57 @@
+exports.config = {
+
+    onPrepare: function(){
+        var jasmineReporters = require('jasmine-reporters');
+        jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter({
+            consolidateAll: true,
+            savePath: './',
+            filePrefix: 'xmlresults'
+        }));
+    },
+
+    onComplete: function() {
+        var browserName, browserVersion;
+        var capsPromise = browser.getCapabilities();
+        capsPromise.then(function (caps) {
+           browserName = caps.get('browserName');
+           browserVersion = caps.get('version');
+           platform = caps.get('platform');
+            var HTMLReport = require('protractor-html-reporter-2');
+            testConfig = {
+                reportTitle: 'Protractor Test Execution Report',
+                outputPath: './',
+                outputFilename: 'ProtractorTestReport',
+                screenshotPath: './screenshots',
+                testBrowser: browserName,
+                browserVersion: browserVersion,
+                modifiedSuiteName: false,
+                screenshotsOnlyOnFailure: true,
+                testPlatform: platform
+            };
+            new HTMLReport().from('xmlresults.xml', testConfig);
+       });
+    },
+
+    directConnect: true,
+
+    multiCapabilities: [
+        {
+        'browserName': 'chrome'
+        },
+        /* {
+         'browserName': 'firefox'
+        }, */
+
+    ], maxSessions: -1, //change to 1 if want to run one after another
+
+    framework: 'jasmine',
+
+    specs: ['..//tests//sample1.js',
+
+            ],
+
+
+    jasmineNodeOpts: {
+        defaultTimeoutInterval: 30000
+    }
+};
