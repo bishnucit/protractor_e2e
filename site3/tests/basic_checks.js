0cf31116b6f1a1da169f2809defd20fdf4f4f37e
Bishnu Prasad Chowdhury <bishnucit@gmail.com>
2021-11-12 10:56:15 +0530

    Update basic_checks.js

--- site3/tests/basic_checks.js
+++ site3/tests/basic_checks.js
@@ -61,11 +61,10 @@ describe("Verify everything", function(){
             browser.sleep(2000);
             expect(element(by.xpath('/html/body/app-root/app-layout-header/nav/div/ul/li[2]/a')).isDisplayed());
         });
-
+        //add new article
         element(by.xpath('/html/body/app-root/app-layout-header/nav/div/ul/li[2]/a')).click();
         expect(element(by.buttonText('Publish Article')).isDisplayed());
 
-        //add new article
         element(by.xpath('/html/body/app-root/app-editor-page/div/div/div/div/form/fieldset/fieldset[1]/input')).sendKeys('article_title_' + today);
         element(by.xpath('/html/body/app-root/app-editor-page/div/div/div/div/form/fieldset/fieldset[2]/input')).sendKeys('test1');
         element(by.xpath('/html/body/app-root/app-editor-page/div/div/div/div/form/fieldset/fieldset[3]/textarea')).sendKeys('article_description_sample_text');
@@ -73,11 +72,44 @@ describe("Verify everything", function(){
         element(by.buttonText('Publish Article')).click();
         browser.sleep(5000);
         expect(element(by.css('.btn-outline-danger')).isDisplayed());
-        
+
         element(by.xpath('/html/body/app-root/app-layout-header/nav/div/ul/li[3]/a')).click();
         browser.sleep(3000);
         element(by.css('.btn.btn-outline-danger')).click();
         browser.sleep(2000);
         expect(element(by.xpath('/html/body/app-root/app-layout-header/nav/div/ul/li[3]/a')).isDisplayed());
     });
+
+    it("TC004 - Login with the registered user and update your settings", function(){
+        element(by.partialLinkText('Sign in')).click();
+        browser.sleep(1000);
+        element(by.xpath('/html/body/app-root/app-auth-page/div/div/div/div/form/fieldset/fieldset[2]/input')).sendKeys('tester1_'+ today + '@mailinator.com');
+        element(by.xpath('/html/body/app-root/app-auth-page/div/div/div/div/form/fieldset/fieldset[3]/input')).sendKeys('Passw0rd123');
+        browser.sleep(1000).then(function(){
+            signin_button = element(by.buttonText('Sign in'))
+            expect(signin_button.isEnabled());
+            signin_button.click();
+            browser.sleep(2000);
+            expect(element(by.xpath('/html/body/app-root/app-layout-header/nav/div/ul/li[2]/a')).isDisplayed());
+        });
+        //update profile
+        element(by.xpath('/html/body/app-root/app-layout-header/nav/div/ul/li[3]/a')).click();
+        browser.sleep(2000);
+        expect(element(by.buttonText('Update Settings')).isDisplayed());
+
+        element(by.xpath('/html/body/app-root/app-settings-page/div/div/div/div/form/fieldset/fieldset[2]/input')).clear().sendKeys('username1_' + today);
+        element(by.xpath('/html/body/app-root/app-settings-page/div/div/div/div/form/fieldset/fieldset[3]/textarea')).clear().sendKeys('my bio here');
+        element(by.xpath('/html/body/app-root/app-settings-page/div/div/div/div/form/fieldset/fieldset[5]/input')).clear().sendKeys('Test@12345');
+        expect(element(by.buttonText('Update Settings')).click());
+        browser.sleep(2000);
+        expect(element(by.xpath('/html/body/app-root/app-layout-header/nav/div/ul/li[2]/a')).isDisplayed());
+
+        element(by.xpath('/html/body/app-root/app-layout-header/nav/div/ul/li[3]/a')).click();
+        browser.sleep(3000);
+        element(by.css('.btn.btn-outline-danger')).click();
+        browser.sleep(2000);
+        expect(element(by.xpath('/html/body/app-root/app-layout-header/nav/div/ul/li[3]/a')).isDisplayed());
+
+    });
+
 });
