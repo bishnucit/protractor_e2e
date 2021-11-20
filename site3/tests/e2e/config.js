d47e270c4572450c4a32c5c92d23d34e74e51e67
Bishnu Prasad Chowdhury <bishnucit@gmail.com>
2021-11-20 10:44:12 +0530

    Create config.js

---
+++ site3/tests/e2e/config.js
@@ -0,0 +1,48 @@
+exports.config = {
+
+    onPrepare: function(){
+        browser.driver.manage().window().setSize(1600, 800);
+        var jasmineReporters = require('jasmine-reporters');
+        jasmine.getEnv().addReporter(new jasmineReporters.JUnitXmlReporter({
+            consolidateAll: true,
+            savePath: './',
+            filePrefix: 'xmlresults'
+        }));
+    },
+
+    onComplete: function() {
+        var browserName, browserVersion;
+        var capsPromise = browser.getCapabilities();
+        capsPromise.then(function (caps) {
+           browserName = caps.get('browserName');
+           browserVersion = caps.get('version');
+           platform = caps.get('platform');
+            var HTMLReport = require('protractor-html-reporter-2');
+            testConfig = {
+                reportTitle: 'Protractor Test Execution Report',
+                outputPath: './',
+                outputFilename: 'ProtractorTestReport',
+                screenshotPath: './screenshots',
+                testBrowser: browserName,
+                browserVersion: browserVersion,
+                modifiedSuiteName: false,
+                screenshotsOnlyOnFailure: true,
+                testPlatform: platform
+            };
+            new HTMLReport().from('xmlresults.xml', testConfig);
+       });
+    },
+
+    directConnect: true,
+
+
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    capabilities: {
+      'browserName': 'chrome'
+    },
+
+    specs: ['..//e2e//signin_page//signinpage.spec.js'],
+    jasmineNodeOpts: {
+      showColors: true
+    }
+  };
