4e865ba43c9f9701c17fb0e14934c4950063bdd5
Bishnu Prasad Chowdhury <bishnucit@gmail.com>
2021-11-21 10:12:26 +0530

    Create signuppage.po.js

---
+++ site3/tests/e2e/signup_page/signuppage.po.js
@@ -0,0 +1,12 @@
+var SignUppage = function(){
+    this.conduitLink = element(by.partialLinkText('conduit'));
+    this.homeLink = element(by.partialLinkText('Home'));
+    this.signinLink = element(by.partialLinkText('Sign in'));
+    this.signupLink = element(by.partialLinkText('Sign up'));
+    this.usernameInput = element(by.css('[ng-reflect-name="username"]'));
+    this.emailInput = element(by.css('[ng-reflect-name="email"]'));
+    this.passInput = element(by.css('[ng-reflect-name="password"]'));
+    this.signinButton = element(by.buttonText('Sign up'));
+    this.needaccountLink = element(by.linkText('Have an account?'));
+};
+module.exports = SignUppage;
