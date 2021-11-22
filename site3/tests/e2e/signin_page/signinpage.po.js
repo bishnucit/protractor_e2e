566f2771fb785923d98f10a4df85ea7e2e7e0aa7
Bishnu Prasad Chowdhury <bishnucit@gmail.com>
2021-11-22 12:54:52 +0530

    Update signinpage.po.js

--- site3/tests/e2e/signin_page/signinpage.po.js
+++ site3/tests/e2e/signin_page/signinpage.po.js
@@ -1,4 +1,8 @@
 var SignInpage = function(){
+    this.conduitLink = element(by.partialLinkText('conduit'));
+    this.homeLink = element(by.partialLinkText('Home'));
+    this.signinLink = element(by.partialLinkText('Sign in'));
+    this.signupLink = element(by.partialLinkText('Sign up'));
     this.emailInput = element(by.css('[ng-reflect-name="email"]'));
     this.passInput = element(by.css('[ng-reflect-name="password"]'));
     this.signinButton = element(by.buttonText('Sign in'));
