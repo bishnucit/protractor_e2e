0f76709b41064f017a64b9c6aa58755a8ff7da7d
Bishnu Prasad Chowdhury <bishnucit@gmail.com>
2021-11-30 13:04:08 +0530

    Update settingspage.po.js

--- site3/tests/e2e/settings_page/settingspage.po.js
+++ site3/tests/e2e/settings_page/settingspage.po.js
@@ -1,4 +1,10 @@
 var Settingspage = function(){
+    this.imageURLTextBox = element(by.css('[ng-reflect-name="image"]'));
+    this.usernameTextBox = element(by.css('[ng-reflect-name="username"]'));
+    this.bioTextBox = element(by.css('[ng-reflect-name="bio"]'));
+    this.emailTextBox = element(by.css('[ng-reflect-name="email"]'));
+    this.passTextBox = element(by.css('[ng-reflect-name="password"]'));
+    this.updateButton = element(by.css('.btn-primary.pull-xs-right'));
     this.logoutButton = element(by.css('.btn-outline-danger'));
 };
 module.exports = Settingspage;
