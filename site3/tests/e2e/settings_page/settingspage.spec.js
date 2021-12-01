08689502faef86315b24cab1cccb08e5d157b981
Bishnu Prasad Chowdhury <bishnucit@gmail.com>
2021-12-01 21:12:07 +0530

    Update settingspage.spec.js

--- site3/tests/e2e/settings_page/settingspage.spec.js
+++ site3/tests/e2e/settings_page/settingspage.spec.js
@@ -1,15 +1,17 @@
 var SignInpage = require('../signin_page/signinpage.po.js');
 var SignUppage = require('../signup_page/signuppage.po.js');
 var Homepage = require('../home_page/homepage.po.js');
+var Articlepage = require('../article_page/articlepage.po.js');
 var SettingsPage = require('./settingspage.po.js');
 
-describe("Sign in page tests", function(){
+describe("Settings page tests", function(){
     browser.ignoreSynchronization = true;
     var EC = protractor.ExpectedConditions;
     var signInpage = new SignInpage();
     var signUppage = new SignUppage();
     var homepage = new Homepage();
     var settingspage = new SettingsPage();
+    var articlepage = new Articlepage();
     //calling global variable declared in config file
     var username = browser.params.username;
     var email = username+'@mailinator.com';
@@ -18,6 +20,7 @@ describe("Sign in page tests", function(){
     var homeURL = 'http://localhost:4200/';
     var loginURL = 'http://localhost:4200/login';
     var settingsURL = 'http://localhost:4200/settings';
+    var editorURL = 'http://localhost:4200/editor';
 
 
     beforeAll(function(){
@@ -53,7 +56,34 @@ describe("Sign in page tests", function(){
     });
 
     it("Redirection verification", function(){
-        //coming soon
+        settingspage.conduitLink.click()
+        .then(function(){
+            browser.wait(EC.presenceOf(homepage.sidebarLink), 5000);
+            expect(browser.getCurrentUrl()).toEqual(homeURL);
+        })
+        .then(()=>(browser.navigate().back()));
+        browser.wait(EC.presenceOf(settingspage.logoutButton), 5000);
+        browser.sleep(2000);
+
+        settingspage.homeLink.click()
+        .then(function(){
+            browser.wait(EC.presenceOf(homepage.sidebarLink), 5000);
+            expect(browser.getCurrentUrl()).toEqual(homeURL);
+        })
+        .then(()=>(browser.navigate().back()));
+        browser.wait(EC.presenceOf(settingspage.logoutButton), 5000);
+        browser.sleep(2000);
+
+        settingspage.newarticleLink.click()
+        .then(function(){
+            browser.wait(EC.presenceOf(articlepage.publisharticleButton), 5000);
+            expect(browser.getCurrentUrl()).toEqual(editorURL);
+        })
+        .then(()=>(browser.navigate().back()));
+        browser.wait(EC.presenceOf(settingspage.logoutButton), 5000);
+        browser.sleep(2000);
     });
+
+
 
 });
