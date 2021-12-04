d922e6812f93f1dda2196460b66003349f8c7a46
Bishnu Prasad Chowdhury <bishnucit@gmail.com>
2021-12-04 09:33:25 +0530

    Update signuppage.po.js

--- site3/tests/e2e/signup_page/signuppage.po.js
+++ site3/tests/e2e/signup_page/signuppage.po.js
@@ -9,6 +9,8 @@ var SignUppage = function(){
     this.passInput = element(by.css('[ng-reflect-name="password"]'));
     this.signupButton = element(by.buttonText('Sign up'));
     this.needaccountLink = element(by.linkText('Have an account?'));
+    this.errorMessage1 = element(by.css('.error-messages'));
+    this.errorMessage2 = element(by.css('.error-messages>li'));
 };
 
 module.exports = SignUppage;
